96b8275bda3623c767d45c469164b3574725f5e2
Никита Синегибский <mykyta.syniehibskyi@vokigames.com>
2025-04-01 21:52:30 +0200

    импорт в мейн

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 # Основной скрипт для запуска калькулятора
+from calculator.calculator.basic_calc import BasicCalc, CalcWithMemory
+
+def main():
+    calc = CalcWithMemory()
+    print(calc.add(1, 2))
+
+if __name__ == "__main__":
+    main()
+
