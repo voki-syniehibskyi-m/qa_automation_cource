91c6015c3cfd502d198eeb0523b1399971282d27
Никита Синегибский <mykyta.syniehibskyi@vokigames.com>
2025-04-06 15:16:47 +0200

    Выполнение практического задания #3

--- calculator/calculator/basic_calc.py
+++ calculator/calculator/basic_calc.py
@@ -1,10 +1,12 @@
 import re
 
-class BasicCalc:
+class ZeroDivisionCatcher(ZeroDivisionError):
+    def __init__(self, message = 'На ноль делить не стоит, введите число > 0'):
+        super().__init__(message)
 
+class BasicCalc:
     @staticmethod
     def add(a, b=None):
-        # функция сложения обновлена, если 1 аргумент, то ждёт итерируемый объект.
         if b is None:
             res = 0
             for i in a:
@@ -22,17 +24,18 @@ class BasicCalc:
 
     @staticmethod
     def divide(a, b):
-        return a / b
+        try:
+            if b == 0:
+                raise ZeroDivisionCatcher
+            else:
+                return a / b
+        except ZeroDivisionCatcher as e:
+            print(f'{e}')
+
+
 
     @staticmethod
-    def _number_checker(num_str):
-        if '.' in num_str:
-            return float(num_str)
-        else:
-            return int(num_str)
-
-    @staticmethod
-    def calculator():
+    def calculate_user_input():
         math_operations = {
             '+': BasicCalc.add,
             '-': BasicCalc.subtract,
@@ -49,8 +52,8 @@ class BasicCalc:
             else:
                 print('Введено неверное выражение, повторите ввод')
 
-        first_num = BasicCalc._number_checker(string_to_match.group(1))
-        second_num = BasicCalc._number_checker(string_to_match.group(4))
+        first_num = float(string_to_match.group(1))
+        second_num = float(string_to_match.group(4))
         math_symbol = string_to_match.group(3)
 
         return math_operations.get(math_symbol)(first_num, second_num)
@@ -65,58 +68,82 @@ class CalcWithMemory(BasicCalc):
         """
         self.memory = []
 
+    """Если введёно не число, то заменяем на 0"""
+    def _number_validator(self, a):
+        return a if isinstance(a, (int, float)) else 0
+
     def memo_plus(self, value):
+        if not isinstance(value, (int, float)):
+            raise ValueError(f'Сохранять можно только числа, получено {value}')
         if len(self.memory) >= 3:
             self.memory.pop(0)
-            self.memory.append(value)
-        else:
-            self.memory.append(value)
+        self.memory.append(value)
 
     def memo_minus(self):
+        if not self.memory:
+            raise IndexError('Память пуста, нечего удалять')
         return self.memory.pop()
 
 
     def add(self, a, b = None):
         if b is None:
-            b = self.memory[-1]
+            if self.memory:
+                b = self.memory[-1]
+            else:
+                raise ValueError('Второй аргумент не задан и память пуста.')
+
+        a, b = map(self._number_validator, (a,b))
         result = a + b
         self.memo_plus(result)
         return result
 
     def subtract(self, a, b = None):
         if b is None:
-            b = self.memory[-1]
+            if self.memory:
+                b = self.memory[-1]
+            else:
+                raise ValueError('Второй аргумент не задан и память пуста.')
+
+        a, b = map(self._number_validator, (a, b))
         result = a - b
         self.memo_plus(result)
         return result
 
     def divide(self, a, b = None):
-        if b is None:
-            b = self.memory[-1]
-        result = a / b
-        self.memo_plus(result)
-        return result
+        try:
+            if b is None:
+                if self.memory:
+                    b = self.memory[-1]
+                else:
+                    raise ValueError('Второй аргумент не задан и память пуста.')
+
+            a, b = map(self._number_validator, (a,b))
+            if b == 0:
+                raise ZeroDivisionCatcher('Попытка деления на 0!')
+            result = a / b
+            self.memo_plus(result)
+            return result
+        except ZeroDivisionCatcher as e:
+            print(f'{e}')
+            return None
+
 
     def multiply(self, a, b = None):
         if b is None:
-            b = self.memory[-1]
+            if self.memory:
+                b = self.memory[-1]
+            else:
+                raise ValueError('Второй аргумент не задан и память пуста.')
+
+        a, b = map(self._number_validator, (a,b))
+
         result = a * b
         self.memo_plus(result)
         return result
 
     @property
     def last_value(self):
-        return self.memory[-1] if self.memory else None
-
-test = CalcWithMemory()
-
-test.add(5,5)
-# test.subtract(10, 5)
-# test.add(1,1)
-print(test.divide(10, ))
-
-print(test.memory)
-
-print(test.last_value)
-
+        if not self.memory:
+            raise IndexError('Память пуста, удалить нечего.')
+        return self.memory[-1]
 
